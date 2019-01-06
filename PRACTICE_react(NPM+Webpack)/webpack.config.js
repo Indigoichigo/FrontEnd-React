97942e7c188fdc93262a422a318cc8cd08a2d75e
Indigoichigo <silentice1534@gmail.com>
2019-01-06 21:30:20 +0800

    0106 2130 setup env

--- PRACTICE_react(NPM+Webpack)/webpack.config.js
+++ PRACTICE_react(NPM+Webpack)/webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: './src/simpleBlog-App/app.js',
+    entry: './src/app.js',
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'bundle.js'
---
+++ UDEMY_react/src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import Hello from './Hello.js';
+
+const Hello = () => {
+    return (
+        <div>Hello, React</div>
+    );
+}
+
+ReactDOM.render(Hello, document.getElementById('root'));
